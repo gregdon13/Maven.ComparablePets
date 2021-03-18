58b8358365cf8d3d3f833651c65065448e758730
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-17 22:55:23 -0400

    Started writing tests. Part 1 should be complete

--- src/main/java/io/zipcoder/Application.java
+++ src/main/java/io/zipcoder/Application.java
@@ -1,5 +1,24 @@
 package io.zipcoder;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
+    Pet pet = new Pet();
+    Map<String, String> userPetMap = new HashMap<String, String>();
+
+    public Integer getNumberOfPets() {
+        return null;
+    }
+
+    public void setNumberOfPets(int numOfPets) {
+    }
+
+    public void setPetNameType(String name, String petType) {
+    }
+
+    public Map getUserPets() {
+        return userPetMap;
+    }
 }
--- src/test/java/io/zipcoder/ApplicationTest.java
+++ src/test/java/io/zipcoder/ApplicationTest.java
@@ -1,5 +1,39 @@
 package io.zipcoder;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApplicationTest {
+    Application app = new Application();
+
+    @Test
+    public void numberOfPets() {
+        //Given
+        app.setNumberOfPets(3);
+        Integer expected = 3;
+
+        //When
+        Integer actual = app.getNumberOfPets();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void petNameAndType() {
+        //Given
+        app.setPetNameType("Fluffy", "cat");
+        String expected = "{Fluffy=cat}";
+
+        //When
+        String actual = app.getUserPets().toString();
+
+        //Actual
+        Assert.assertEquals(expected, actual);
+    }
 }
